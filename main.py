43fb83fabdae7a1d456dce2e474fa606f4843a98
Emberash0 <christopher.a.wareham@gmail.com>
2025-12-26 22:41:20 +0000

    API key handling

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from mephistophai!")
+import os
+from dotenv import load_dotenv
+from google import genai
 
+
+def main():
+    #grabs API key from .env and declares error if no key found
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key == None:
+        raise RuntimeError("No API key found")
 
 if __name__ == "__main__":
     main()
